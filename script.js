518c5457a5e7c7b66cafaf0d83ff79ec5364664a
Clement Kalu Okereke <inspikalu@gmail.com>
2024-11-18 13:55:59 +0100

    Update

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-const form = document.getElementById("feedbackForm")
+const form = document.getElementById("requestForm")
 const submitButton = document.getElementById("submitButton")
-submitButton.addEventListener("click", () => {
-    navigator.geolocation.getCurrentPosition(
-        (position) => {
-            console.log("Current position", position);
-        },
-        (error) => {
-            console.error("Error code:", error.code);
-            console.error("Error message:", error.message);
-        }
-    );
+form.addEventListener("submit", () => {
+    try {
+
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log("Current position", position);
+            },
+            (error) => {
+                console.error("Error code:", error.code);
+                console.error("Error message:", error.message);
+            }
+        );
+    } catch (error) {
+        console.log(error)
+        fetch('http://ip-api.com/json')
+            .then(response => response.json())
+            .then(data => {
+                console.log('IP Geolocation:', data);
+                // Output will include fields like lat, lon, etc.
+            })
+            .catch(error => {
+                console.error('Error fetching geolocation data:', error);
+            });
+    }
+
+
+
 });
